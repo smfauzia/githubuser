7ef0109b8e60cf23fafc4c542bd2d96d8920c9ca
fauzi <muhfauziaulia@gmail.com>
2021-06-10 13:31:55 +0700

    update fungsi

--- app/src/main/java/com/latihan/githubuser/MainActivity.kt
+++ app/src/main/java/com/latihan/githubuser/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.View
 import android.widget.Toast
